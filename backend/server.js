632afffc7c513419490db3ce332c28432514c7ad
Ankit BagheL <ak9664773@gmail.com>
2024-03-05 17:42:10 +0530

    frontend routing and backend models done

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,20 @@
 import express from "express"
+import dotenv from "dotenv"
+import { chats } from "./data/data.js";
 
 const app = express();
+dotenv.config();
 
 app.get("/", (req, res) => {
-    res.send("hello")
+    res.send(chats)
 })
 
-app.listen(8080, () => {
+app.get("/api/:id", (req, res) => {
+    const list = chats.find((c) => c._id === req.params.id)
+    res.send(list);
+})
+// app.get("api/chat",)
+
+app.listen(process.env.PORT, () => {
     console.log("server is running on port 8080..");
 })
